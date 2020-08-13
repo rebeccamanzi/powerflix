49c71b774f1e85126c22c61873454c1b935f3216
rebecca-ajl <rebeccamanzi@gmail.com>
2020-08-13 16:42:14 -0300

    style reset

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import Menu from '../Menu';
 import Footer from '../Footer';
+import styled from 'styled-components';
 
-function PageDefault() {
+const Main = styled.main`
+  background-color: var(--black);
+  color: var(--white);
+  flex: 1;
+  padding-top: 50px;
+  padding-right: 5%;
+  /* variáveis que foram definidas no reset do index */
+`;
+
+function PageDefault({ children }) {
   return (
-    <div>
-      <Menu />
-  
-      <Footer />
-    </div>
+    <>
+    <Menu />
+      <Main>
+        {children}
+      </Main>
+    <Footer />
+    </>
   );
 }
 
 export default PageDefault;
+
+// {children} -> desconstrução do props
+// renderiza o que for adicionado dentro do componente
+
+// styled components -> css dentro do js
+// padroniza o estilo em todas as utilizações deste componente
+
+// <> </> ou <React.Fragment> </React.Fragment>
+// pega o fragmento que está dentro do React
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './reset.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={'Pagina404'} />
     </Switch>
   </BrowserRouter>,
